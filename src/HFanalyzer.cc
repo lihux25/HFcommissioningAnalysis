8573521c6b532c94707053054f61282bc259046d
Ohannes Kamer Koseyan <kamer.koseyan@cern.ch>
2016-02-08 12:00:17 +0100

    fixing bugs related to type casting the dataframe

--- src/HFanalyzer.cc
+++ src/HFanalyzer.cc
@@ -301,20 +301,20 @@ void HFanalyzer::getData(const edm::Event &iEvent,
 
   if (_verbosity>0) std::cout << "Trying to access the qie collection" << std::endl;
     
-  for (int j=0; j < qie10dc.size(); j++){
+  for( unsigned int j=0; j < qie10dc.size(); j++){
 
     QIE10DataFrame qie10df = static_cast<QIE10DataFrame>(qie10dc[j]);
 
     if (_verbosity>0){
       std::cout << "Printing raw dataframe" << std::endl;
-      std::cout << qie10dc[j] << std::endl;
+      std::cout << qie10df << std::endl;
             
       std::cout << "Printing content of samples() method" << std::endl;
-      std::cout << qie10dc[j].samples() << std::endl;
+      std::cout << qie10df.samples() << std::endl;
     }
         
     // Extract info on detector location
-    DetId detid = qie10dc[j].detid();
+    DetId detid = qie10df.detid();
     HcalDetId hcaldetid = HcalDetId(detid);
     int ieta = hcaldetid.ieta();
     int iphi = hcaldetid.iphi();
@@ -353,7 +353,7 @@ void HFanalyzer::getData(const edm::Event &iEvent,
     }
 
     // loop over the samples in the digi
-    int nTS = qie10dc[j].samples();
+    int nTS = qie10df.samples();
 
     float ped_adc = 0;
     float ped_fc = 0;
@@ -374,10 +374,10 @@ void HFanalyzer::getData(const edm::Event &iEvent,
 
 	// j - QIE channel
 	// i - time sample (TS)
-	int adc = qie10dc[j][i].adc();
-	int tdc = qie10dc[j][i].le_tdc();
-	int capid = qie10dc[j][i].capid();
-	int soi = qie10dc[j][i].soi();
+	int adc = qie10df[i].adc();
+	int tdc = qie10df[i].le_tdc();
+	int capid = qie10df[i].capid();
+	int soi = qie10df[i].soi();
 
 	// store pulse information
 	float charge = adc2fC_QIE10[ adc ];
@@ -425,7 +425,7 @@ void HFanalyzer::getData(const edm::Event &iEvent,
     _qie10Info.depth[j] = depth;
     _qie10Info.ped[j] = ped_fc;
     _qie10Info.ped_adc[j] = ped_adc;
-    _qie10Info.link_error[j] = qie10dc[j].linkError();
+    _qie10Info.link_error[j] = qie10df.linkError();
   }
 
   _qie10Info.numChs = qie10dc.size();
